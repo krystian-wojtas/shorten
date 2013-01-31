4115d4f24b968907ef3e910eaa66a8345ad05df8
Krystian Wojtas <krystian.wojtas@gmail.com>
2013-01-31 16:48:49 +0100

    validates presence of longer and shorter fields

--- app/models/short.rb
+++ app/models/short.rb
@@ -2,8 +2,7 @@ class Short < ActiveRecord::Base
   attr_accessible  :longer, :shorter
   attr_defaults :chrome => 0, :firefox => 0, :ie => 0, :opera => 0, :other => 0, :safari => 0
 
-  #TODO longer shorter not blank
-  
-  validates_uniqueness_of :shorter
+  validates :shorter, :presence => true, :uniqueness => true
+  validates :longer, :presence => true
   validates_format_of :longer, :with => URI::regexp(['http'])
 end
