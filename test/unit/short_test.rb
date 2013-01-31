63a877c65e5390334865af386bb85cf10f42bc73
Krystian Wojtas <krystian.wojtas@gmail.com>
2013-01-31 19:16:33 +0100

    test f function

--- test/unit/short_test.rb
+++ test/unit/short_test.rb
@@ -4,4 +4,8 @@ class ShortTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "f randomizable" do
+    short = shorts(:railsguide)
+    assert short.f() != short.f(), "Randomizing doesnt work"
+  end
 end
