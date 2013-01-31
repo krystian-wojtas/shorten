e6094f05b9afded5587774d821bd6a9385f9b448
Krystian Wojtas <krystian.wojtas@gmail.com>
2013-01-31 17:51:20 +0100

    busy scope in Short model

--- app/models/short.rb
+++ app/models/short.rb
@@ -2,10 +2,12 @@ class Short < ActiveRecord::Base
   attr_accessible  :longer, :shorter
   attr_defaults :chrome => 0, :firefox => 0, :ie => 0, :opera => 0, :other => 0, :safari => 0
 
+  scope :busy, lambda { |short| where('shorter = ?', short).limit(1) }
+  
   before_validation do |short|
     begin
       self.shorter = longer.hash + rand.hash
-    end until(Short.where('shorter = ?', self.shorter).empty?)
+    end until(Short.busy(self.shorter).empty?)
   end
 
   validates :shorter, :presence => true, :uniqueness => true
